b2d88e9d772d3d2aeb5d99afd4e9cc6aab68c582
ech0 <hello@ech0.dev>
2025-01-02 21:17:46 -0500

    test commit

--- app/src/main/java/dev/ech0/torbox/MainActivity.kt
+++ app/src/main/java/dev/ech0/torbox/MainActivity.kt
@@ -38,7 +38,7 @@ import kotlinx.coroutines.launch
 val LocalNavController = compositionLocalOf<NavController> { error("No NavController found!") }
 
 class MainActivity : ComponentActivity() {
-
+    // meow
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val intent = intent
