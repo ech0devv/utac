ea4bfb5e82f0e9928abee960fb9de784e113675a
ech0x0 <winterarctic26@outlook.com>
2025-01-20 11:00:59 -0500

    v1.4-alpha1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,9 +14,9 @@ android {
     defaultConfig {
         applicationId = "dev.ech0.torbox"
         minSdk = 29
-        targetSdk = 34
-        versionCode = 10
-        versionName = "1.3"
+        targetSdk = 35
+        versionCode = 11
+        versionName = "1.4-alpha1"
         
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
